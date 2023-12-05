d789b9332304a56a60466bb7010514ff1f89289c
SIDANWhatever <wongkahinhinson@gmail.com>
2023-12-05 21:26:53 +0800

    feat: adding aiken param scripts

--- src/aiken/index.ts
+++ src/aiken/index.ts
@@ -2,6 +2,45 @@ import { Data } from "@meshsdk/core";
 import aikenScripts from "./blueprint.json";
 import { C } from "./libs";
 
+const toBytes = (hex: string): Uint8Array => {
+  if (hex.length % 2 === 0 && /^[0-9A-F]*$/i.test(hex)) return Buffer.from(hex, "hex");
+
+  return Buffer.from(hex, "utf-8");
+};
+
+const toPlutusData = (data: Data) => {
+  const toPlutusList = (data: Data[]) => {
+    const plutusList = C.PlutusList.new();
+    data.forEach((element) => {
+      plutusList.add(toPlutusData(element));
+    });
+
+    return plutusList;
+  };
+
+  switch (typeof data) {
+    case "string":
+      return C.PlutusData.new_bytes(toBytes(data));
+    case "number":
+      return C.PlutusData.new_integer(C.BigInt.from_str(data.toString()));
+    case "object":
+      if (data instanceof Array) {
+        const plutusList = toPlutusList(data);
+        return C.PlutusData.new_list(plutusList);
+      } else if (data instanceof Map) {
+        const plutusMap = C.PlutusMap.new();
+        data.forEach((value, key) => {
+          plutusMap.insert(toPlutusData(key), toPlutusData(value));
+        });
+        return C.PlutusData.new_map(plutusMap);
+      } else {
+        return C.PlutusData.new_constr_plutus_data(
+          C.ConstrPlutusData.new(C.BigNum.from_str(data.alternative.toString()), toPlutusList(data.fields))
+        );
+      }
+  }
+};
+
 export const toHex = (bytes: Uint8Array) => Buffer.from(bytes).toString("hex");
 export const fromHex = (hex: string) => Buffer.from(hex, "hex");
 
@@ -14,14 +53,22 @@ export function applyDoubleCborEncoding(script: string): string {
   }
 }
 
-export function applyParamsToScript(plutusScript: string, params: Array<Data>): string {
-  const plutusData = C.PlutusData.from_json(JSON.stringify(params), 1).to_bytes();
+export function applyParamsToScript(plutusScript: string, params: Data[]): string {
+  const plutusList = C.PlutusList.new();
+  params.forEach((param) => {
+    const plutusData = toPlutusData(param);
+    plutusList.add(plutusData);
+  });
   return toHex(
     C.apply_params_to_plutus_script(
-      C.PlutusList.from_bytes(plutusData),
+      plutusList,
       C.PlutusScript.from_bytes(fromHex(applyDoubleCborEncoding(plutusScript)))
     ).to_bytes()
   );
+}
+
+export function getV2ScriptHash(script: string): string {
+  return C.PlutusScript.from_hex_with_version(script, C.Language.new_plutus_v2()).hash().to_hex();
 }
 
 export const blueprint = aikenScripts;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
-import { InfuraProvider } from "@meshsdk/core";
-import { blueprint, applyParamsToScript } from "../aiken";
+import { InfuraProvider, MaestroProvider, MeshTxBuilder, Data } from "@meshsdk/core";
+import { blueprint, applyParamsToScript, getV2ScriptHash } from "../aiken";
 
 const infura = new InfuraProvider(
   process.env.NEXT_PUBLIC_INFURA_PROJECT_ID!,
   process.env.NEXT_PUBLIC_INFURA_PROJECT_SECRET!,
   {}
 );
+
+const maestro = new MaestroProvider({ apiKey: process.env.NEXT_PUBLIC_MAESTRO_APIKEY!, network: "Preprod" });
+const walletAddress = process.env.NEXT_PUBLIC_WALLET_ADDRESS!;
+const skey = process.env.NEXT_PUBLIC_SKEY!;
 
 export default function Home() {
   async function uploadMarkdown() {
@@ -33,15 +37,79 @@ export default function Home() {
     // create cip68 tokens so that CID is stored on chain and owner can update it
   }
 
-  const script = blueprint.validators[2].compiledCode;
-  const paramScript = applyParamsToScript(script, [
-    [{ bytes: "3fbdf2b0b4213855dd9b87f7c94a50cf352ba6edfdded85ecb22cf9ceb75f814" }],
-  ]);
-  console.log("Param scripts", paramScript);
+  const queryUtxos = async () => {
+    const utxos = await maestro.fetchAddressUTxOs(walletAddress);
+    console.log("UTXOS", utxos);
+    return utxos;
+  };
+
+  const mesh = new MeshTxBuilder({
+    fetcher: maestro,
+    submitter: maestro,
+    evaluator: maestro,
+  });
+
+  const mintOneTimeMintingPolicy = async () => {
+    const paramScript = applyParam();
+    const policyId = getV2ScriptHash(paramScript);
+    const tokenName = "";
+    console.log("Policy ID", policyId);
+
+    await mesh
+      .txIn("3fbdf2b0b4213855dd9b87f7c94a50cf352ba6edfdded85ecb22cf9ceb75f814", 6)
+      .txOut(walletAddress, [
+        { unit: "lovelace", quantity: "5000000" },
+        { unit: policyId + tokenName, quantity: "1" },
+      ])
+      .mintPlutusScriptV2()
+      .mint(1, policyId, tokenName)
+      .mintingScript(paramScript)
+      .mintRedeemerValue({ alternative: 0, fields: [] })
+      .txInCollateral("3fbdf2b0b4213855dd9b87f7c94a50cf352ba6edfdded85ecb22cf9ceb75f814", 7)
+      .changeAddress(walletAddress)
+      .signingKey(skey)
+      .complete();
+
+    const signedTx = mesh.completeSigning();
+    const txHash = await mesh.submitTx(signedTx);
+    console.log("TX HASH", txHash);
+  };
+
+  const applyParam = () => {
+    const script = blueprint.validators[2].compiledCode;
+    const param: Data = {
+      alternative: 0,
+      fields: [{ alternative: 0, fields: ["3fbdf2b0b4213855dd9b87f7c94a50cf352ba6edfdded85ecb22cf9ceb75f814"] }, 6],
+    };
+    const paramScript = applyParamsToScript(script, [param]);
+    console.log("Param scripts", paramScript);
+    return paramScript;
+  };
+
+  // console.log("Param scripts", paramScript);
 
   return (
     <main>
-      <button onClick={() => uploadMarkdown()}>upload</button>
+      <button onClick={() => applyParam()}>Apply</button>
+      <button onClick={() => mintOneTimeMintingPolicy()}>mint</button>
+      <button onClick={() => queryUtxos()}>Query</button>
     </main>
   );
 }
+
+// {
+//   "address": "addr_test1vpw22xesfv0hnkfw4k5vtrz386tfgkxu6f7wfadug7prl7s6gt89x",
+//   "tx_hash": "3fbdf2b0b4213855dd9b87f7c94a50cf352ba6edfdded85ecb22cf9ceb75f814",
+//   "tx_index": 6,
+//   "output_index": 6,
+//   "amount": [
+//     {
+//       "unit": "lovelace",
+//       "quantity": "10000000"
+//     }
+//   ],
+//   "block": "54f8ac30aab85d027b283fd124f969e0f5a0534343235e89e6d946862efe967d",
+//   "data_hash": null,
+//   "inline_datum": null,
+//   "reference_script_hash": null
+// },
