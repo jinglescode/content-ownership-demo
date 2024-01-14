13dc9478b43ec6a7debe2fc9e05effe6c06ed119
Tom.yu <tom.yu@accenture.com>
2024-01-14 13:31:10 +0800

    Component: NewContentPage Header

---
+++ src/components/organism/NewContentHeader/NewContentHeader.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import React from "react";
+
+import Link from "next/link";
+import Icon from "@/components/atom/icon/Icon";
+
+export default function NewContentHeader({
+  title,
+  loading,
+  callback,
+}: {
+  title: string;
+  loading: boolean;
+  callback: () => void;
+}) {
+  return (
+    <div className="border-b">
+      <div className="container flex justify-between items-center p-1.5">
+        <div className="flex space-x-2 text-black items-center">
+          <Link href="/">
+            <Icon/>
+          </Link>
+
+          <p>New Content</p>
+        </div>
+        <button className="bg-black rounded-lg p-1 cursor-pointer" disabled={title.length < 10 || loading} onClick={callback}>
+          {loading ? "Saving.." : "Save"}
+        </button>
+      </div>
+    </div>
+  );
+}
