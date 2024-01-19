71c41fcc4fb9565675c1e295cb216f68a0f219de
Tom.yu <tom.yu@accenture.com>
2024-01-19 13:13:32 +0800

    Page: User page update
    adding selecting Asset button

--- src/pages/user/page.tsx
+++ src/pages/user/page.tsx
@@ -1,15 +1,17 @@
-import TextField from '@/components/atom/Textfield/Textfield'
-import Header from '@/components/organism/Header/Header'
-import UserProfile from '@/components/organism/UserProfile/UserProfile'
-import React from 'react'
+import SelectAssestButton from "@/components/organism/SelectAssestButton/SelectAssestButton";
+import TextField from "@/components/atom/Textfield/Textfield";
+import Header from "@/components/organism/Header/Header";
+import UserProfile from "@/components/organism/UserProfile/UserProfile";
+import React from "react";
 
-function User(walletAddress:String) {
+function User(walletAddress: String) {
   return (
     <div>
-        <Header/>
-        <UserProfile/>
+      <Header />
+      {/* {<UserProfile />} */}
+      <SelectAssestButton/>
     </div>
-  )
+  );
 }
 
-export default User
+export default User;
