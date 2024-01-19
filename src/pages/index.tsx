da7d78f1d1a5910add1b5f55423b7b37c0aee2ac
Tom.yu <tom.yu@accenture.com>
2024-01-19 17:18:00 +0800

    Redux : Saving WalletData to redux when connect to wallet in HomePage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,55 +6,138 @@ import { Provider, useDispatch, useSelector } from "react-redux";
 
 import store, { AppDispatch } from "@/redux/store";
 import { useEffect, useState } from "react";
-import { MeshProvider } from "@meshsdk/react";
+import { MeshProvider, useWallet } from "@meshsdk/react";
 import { RootReducer } from "@/redux/rootReducer";
+import Page from "./NewContent/page";
+import PostTable from "@/components/organism/PostTable/PostTable";
+import axios from "axios";
+import TransferContentButton from "@/components/organism/TransferContentButton/TransferContentButton";
+import {
+  addWalletAddress,
+  updateWalletAddress,
+} from "@/redux/actions/walletAddress";
+import { addFeeUtxo, updateFeeUtxo } from "@/redux/actions/feeUtxo";
+import {
+  addCollateralUtxo,
+  updateCollateralUtxo,
+} from "@/redux/actions/collateralUtxo";
+import { addAsset, updateAsset } from "@/redux/actions/asset";
+import { addGetContentData, updateGetContentData } from "@/redux/actions/getContentData";
+import { addAssetsList, updateAssetsList } from "@/redux/actions/assetsList";
 
 export default function Home() {
-  const [loading, setLoading] = useState<boolean>(false);
-  
+  const [loading, setLoading] = useState(false);
 
-  const [loggedIn, setloggedIn] = useState(false)
- 
-  
-  
+  const [data, setData] = useState<Array<any>>([]);
+  const { connected, wallet } = useWallet();
+  const dispatch: AppDispatch = useDispatch();
+  const walletAddress = useSelector(
+    (state: RootReducer) => state.walletAddress
+  );
+  const feeUtxo = useSelector((state: RootReducer) => state.feeUtxo);
+  const collateralUtxo = useSelector(
+    (state: RootReducer) => state.collateralUtxo
+  );
+  const assetsList =useSelector((state: RootReducer) => state.assetsList);
+  const getContentData = useSelector((state: RootReducer) => state.getContentData);
+
+
+  const savingWalletData = async () => {
+    try {
+      // walletAddress: first item in used address, if no used address, use first item in unused address
+      const address = await wallet.getUsedAddresses();
+      // Get fee UTxO: select an UTxO with >5,000,000 lovelace
+      const utxos = await wallet.getUtxos();
+      const collateral = await wallet.getCollateral();
+      const asset = await wallet.getAssets();
+
+      /**Saving walletAddress */
+      console.log("walletAddress:", address[0]);
+      dispatch(
+        walletAddress.length > 0
+          ? updateWalletAddress(address[0])
+          : addWalletAddress(address[0])
+      );
+
+      /**Saving feeUtxo */
+      console.log("feeUtxo:", utxos[0]);
+      dispatch(
+        feeUtxo.length > 0 ? updateFeeUtxo(utxos[0]) : addFeeUtxo(utxos[0])
+      );
+
+      /**Saving collateralUtxo */
+      console.log("collateralUtxo:", collateral[0]);
+      dispatch(
+        collateralUtxo.length > 0
+          ? updateCollateralUtxo(collateral[0])
+          : addCollateralUtxo(collateral[0])
+      );
+      
+      /**Saving asset */
+      console.log("assetList:", asset);
+      dispatch(
+        assetsList.length > 0
+          ? updateAssetsList(asset)
+          : addAssetsList(asset)
+      );
+    } catch (error) {}
+  };
+
+  const fetchingData = async () => {
+    const res = await axios.get("../api/get-content");
+    
+    dispatch(
+      getContentData.length > 0
+        ? updateGetContentData(res.data)
+        : addGetContentData(res.data)
+    );
+    setData(getContentData);
+    console.log(res.data);
+  };
+
+  //TODO:change it into loading , trigger once only
+  useEffect(() => {
+    fetchingData();
+    savingWalletData()
+  }, [connected]);
 
   return (
-    
-        <div className=" max-w-7xl">
-          <Head>
-            <title>Content-Ownership-demo</title>
-            <link rel="icon" href="/favicon.ico"></link>
-          </Head>
+    <div className=" max-w-7xl">
+      <Head>
+        <title>Content-Ownership-demo</title>
+        <link rel="icon" href="/favicon.ico"></link>
+      </Head>
 
-          <Header />
+      <Header />
 
-          <div className="flex justify-between items-center bg-yellow-400 border-white border-y border-black py-10 lg:py-0">
-            <div className="text-black px-10 space-y-5">
-              <h1 className="text-6xl max-w-xl font-serif ">
-                <span className="underline decoration-black decoration-4">
-                  Medium
-                </span>{" "}
-                is a place to write, read and connect
-              </h1>
-              <h2>
-                It&apos;s easy and free to post your thinking on any topic and
-                connect with millions of readers
-              </h2>
-            </div>
-
-            <Image
-              className="hidden md:inline-flex h-32 lg:h-full"
-              src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png"
-              width={0}
-              height={0}
-              sizes="100vw"
-              style={{ width: "auto", height: "auto" }}
-              alt="medium logo"
-            />
-          </div>
-
-          {/**Post component */}
+      <div className="flex justify-between items-center bg-yellow-400 border-white border-y border-black py-10 lg:py-0">
+        <div className="text-black px-10 space-y-5">
+          <h1 className="text-6xl max-w-xl font-serif ">
+            <span className="underline decoration-black decoration-4">
+              Medium
+            </span>{" "}
+            is a place to write, read and connect
+          </h1>
+          <h2>
+            It&apos;s easy and free to post your thinking on any topic and
+            connect with millions of readers
+          </h2>
         </div>
-     
+
+        <Image
+          className="hidden md:inline-flex h-32 lg:h-full"
+          src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png"
+          width={0}
+          height={0}
+          sizes="100vw"
+          style={{ width: "auto", height: "auto" }}
+          alt="medium logo"
+        />
+      </div>
+      {/**Transfer Content button */}
+      <TransferContentButton />
+      {/**Post component */}
+      <PostTable data={data} />
+    </div>
   );
 }
